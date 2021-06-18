55e26e253789443e27a0c9a9bc6577729bc6344d
laubernal <lbernalrodrguez@gmail.com>
2021-06-18 17:11:20 +0200

    adding different methods to wrapp lstat in a promise and logging the files and if they are files or not

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const util = require('util');
+// const chalk = require('chalk');
+// const path = require('path');
 
+// Method #2 of wrapping lstat in a promise
+// const lstat = util.promisify(fs.lstat);
 
-fs.readdir(process.cwd(), (err, filenames) => {
-    if (err) {
+// Method #3 of wrapping lstat in a promise
+// It can be deconstructed!
+// const lstat = fs.promises.lstat;
+const { lstat } = fs.promises;
+
+// const targetDirectory = process.argv || process.cwd();
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  for (let filename of filenames) {
+      try {
+        const stats = await  lstat(filename);
+
+        console.log(filename, stats.isFile());
+      } catch (err) {
         console.log(err);
-    }
-
-    console.log(filenames);
+      }
+  }
 });
+
+// Method #1 of wrapping lstat in a promise
+/*  const lstat = (filename) => {
+    return new Promise((resolve, reject) => {
+      fs.lstat(filename, (err, stats) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(stats);
+      });
+    });
+  }; */
