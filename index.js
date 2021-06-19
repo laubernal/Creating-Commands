bd3338192550018e6d89827fbebf37663d0e1020
laubernal <lbernalrodrguez@gmail.com>
2021-06-19 13:43:42 +0200

    adding differentiation with colors and paht module

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const util = require('util');
-// const chalk = require('chalk');
-// const path = require('path');
+// const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
 // Method #2 of wrapping lstat in a promise
 // const lstat = util.promisify(fs.lstat);
 
-// Method #3 of wrapping lstat in a promise
-// It can be deconstructed!
-// const lstat = fs.promises.lstat;
+// Method #3 of wrapping lstat in a promise - the module already has a way of dealing with promises
+// It can be deconstructed! -> const lstat = fs.promises.lstat;
 const { lstat } = fs.promises;
 
-// const targetDirectory = process.argv || process.cwd();
+const targetDirectory = process.argv[2] || process.cwd();
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+fs.readdir(targetDirectory, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  for (let filename of filenames) {
-      try {
-        const stats = await  lstat(filename);
+  const statsPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDirectory, filename));
+  });
 
-        console.log(filename, stats.isFile());
-      } catch (err) {
-        console.log(err);
-      }
+  const allStats = await Promise.all(statsPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(chalk.cyan(filenames[index]));
+    } else {
+      console.log(chalk.yellow(filenames[index]));
+    }
   }
 });
 
